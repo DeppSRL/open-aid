cae820c9fe9e8be5ca2a5b23cbe92173aa5b1999
joke2k <joke2k@gmail.com>
2014-06-04 19:35:28 +0200

    fix template bugs

--- project/openaid/settings/production.py
+++ project/openaid/settings/production.py
@@ -16,19 +16,19 @@ ALLOWED_HOSTS = []
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#email-host
-EMAIL_HOST = env('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_HOST = env.str('EMAIL_HOST', 'smtp.gmail.com')
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#email-host-password
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD', '')
+EMAIL_HOST_PASSWORD = env.str('EMAIL_HOST_PASSWORD', '')
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#email-host-user
-EMAIL_HOST_USER = env('EMAIL_HOST_USER', 'daniele.faraglia@gmail.com')
+EMAIL_HOST_USER = env.str('EMAIL_HOST_USER', '{{cookiecutter.email}}')
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#email-port
-EMAIL_PORT = env('EMAIL_PORT', 587)
+EMAIL_PORT = env.int('EMAIL_PORT', 587)
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#email-subject-prefix
-EMAIL_SUBJECT_PREFIX = env('EMAIL_SUBJECT_PREFIX', '[%s] ' % PROJECT_NAME)
+EMAIL_SUBJECT_PREFIX = env.str('EMAIL_SUBJECT_PREFIX', '[%s] ' % PROJECT_NAME)
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#email-use-tls
 EMAIL_USE_TLS = env.bool('EMAIL_USE_TLS', True)
