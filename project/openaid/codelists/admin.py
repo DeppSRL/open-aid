51a261e005e0db91b43f8d9ffd6793fc0a8dac74
joke2k <joke2k@gmail.com>
2014-06-29 16:36:13 +0200

    improve admin for codelists

--- project/openaid/codelists/admin.py
+++ project/openaid/codelists/admin.py
@@ -6,7 +6,28 @@ from django_mptt_admin.admin import DjangoMpttAdmin
 
 class CodeListAdmin(admin.ModelAdmin):
 
-    list_display = ('code', 'name', 'activities', )
+    list_display = ('code', 'name', )
+    extra_list_display = ()
+
+    def  get_list_display(self, request):
+        return super(CodeListAdmin, self).get_list_display(request) + self.extra_list_display
+        # list_display = super(CodeListAdmin, self).get_list_display(request)
+        # all_fields = self.model._meta.get_all_field_names()
+        # if 'activity' in all_fields:
+        #     list_display += ('activities', )
+        # if 'parent' in all_fields:
+        #     list_display += ('level', )
+        # return list_display
+
+
+class ActivityCodeListAdmin(CodeListAdmin):
+
+    extra_list_display = ('activities', )
+
+    def activities(self, obj):
+        return "%s" % obj.activity_count
+    activities.short_description = 'Activities'
+    activities.admin_order_field = 'activity_count'
 
     def activities(self, obj):
         return "%s" % obj.activity_count
@@ -16,8 +37,8 @@ class CodeListAdmin(admin.ModelAdmin):
     def queryset(self, request):
         return super(CodeListAdmin, self).queryset(request).annotate(activity_count=Count('activity'))
 
-
-class HierarchicalCodeListAdmin(DjangoMpttAdmin, CodeListAdmin):
+class HierarchicalCodeListAdmin(DjangoMpttAdmin, ActivityCodeListAdmin):
+    extra_list_display = ActivityCodeListAdmin.extra_list_display + ('level', )
     raw_id_fields = ('parent',)
     search_fields = ('name', 'code', 'description')
     list_filter = ('level', )
@@ -25,18 +46,26 @@ class HierarchicalCodeListAdmin(DjangoMpttAdmin, CodeListAdmin):
 
 class RecipientCodeListAdmin(HierarchicalCodeListAdmin):
 
-    list_display = HierarchicalCodeListAdmin.list_display + (
-        'iso_code', 'iso_alpha2', 'popolazione', 'crescita_popolazione', 'pil', 'pil_procapite',
+    extra_list_display = HierarchicalCodeListAdmin.extra_list_display + (
+        'income_group', 'iso_code', 'iso_alpha2', 'popolazione', 'crescita_popolazione', 'pil', 'pil_procapite',
     )
+    list_filter = HierarchicalCodeListAdmin.list_filter + ('income_group', )
 
-# register all code list to admin
-for code_list_model in models.CODE_LISTS:
-    if 'parent' in code_list_model._meta.get_all_field_names():
-        # it is a hierarchy
-        if code_list_model.code_list == 'recipient':
-            # exception fo recipient
-            admin.site.register(code_list_model, RecipientCodeListAdmin)
-        else:
-            admin.site.register(code_list_model, HierarchicalCodeListAdmin)
-    else:
-        admin.site.register(code_list_model, CodeListAdmin)
+class DonorCodeListAdmin(CodeListAdmin):
+    extra_list_display = ('group', )
+    list_filter = ('group', )
+
+class ChannelCodeListAdmin(HierarchicalCodeListAdmin):
+    extra_list_display = HierarchicalCodeListAdmin.extra_list_display + ('acronym', )
+
+class AgencyCodeListAdmin(CodeListAdmin):
+    extra_list_display = CodeListAdmin.extra_list_display + ('acronym', 'donor')
+
+admin.site.register(models.Donor, DonorCodeListAdmin)
+admin.site.register(models.Agency, ActivityCodeListAdmin)
+
+admin.site.register(models.Recipient, RecipientCodeListAdmin)
+admin.site.register(models.Channel, ChannelCodeListAdmin)
+admin.site.register(models.FinanceType, HierarchicalCodeListAdmin)
+admin.site.register(models.AidType, HierarchicalCodeListAdmin)
+admin.site.register(models.Sector, HierarchicalCodeListAdmin)
