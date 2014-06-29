178f3cf5ffb87a532cbb25dd07d755887812ac0d
joke2k <joke2k@gmail.com>
2014-06-29 19:22:00 +0200

    prepare recipients for main menu, using add_related_count to exclude codelists without activities from main menu

--- project/openaid/contexts.py
+++ project/openaid/contexts.py
@@ -16,14 +16,47 @@ YEARS = range(START_YEAR, END_YEAR+1)
 
 def project_context(request):
 
+    recipients = []
+    for root_recipient in models.Recipient.objects.root_nodes().prefetch_related('children'):
+        children = models.Recipient.objects.add_related_count(
+            root_recipient.get_children(),
+            projects_models.Activity,
+            'recipient', 'activity_count', cumulative=True
+        )
+        setattr(root_recipient, 'activity_count', sum(map(lambda x: x.activity_count, children)))
+        if len(children) == 1:
+            root_recipient = children[0]
+            children = []
+        recipients.append(
+            (root_recipient, children)
+        )
+
+    sectors = models.Sector.objects.add_related_count(
+        models.Sector.objects.root_nodes(),
+        projects_models.Activity,
+        'sector', 'activity_count', cumulative=True
+    )
+
+    channels = models.Channel.objects.add_related_count(
+        models.Channel.objects.root_nodes(),
+        projects_models.Activity,
+        'channel', 'activity_count', cumulative=True
+    )
+
+    aid_types = models.AidType.objects.add_related_count(
+        models.AidType.objects.root_nodes(),
+        projects_models.Activity,
+        'aid_type', 'activity_count', cumulative=True
+    )
+
     return {
         'project_name': settings.PROJECT_NAME,
         'available_languages': map(lambda x: x[0], settings.LANGUAGES),
         'footer_sections': pages_urls.footer_sections,
-        'recipients': models.Recipient.objects.root_nodes().prefetch_related('children'),
-        'sectors': models.Sector.objects.root_nodes(),
-        'channels': models.Channel.objects.root_nodes(),
-        'aids': models.AidType.objects.root_nodes(),
+        'recipients': sorted(recipients, key=lambda r: r[0].name),
+        'sectors': sectors,
+        'channels': channels,
+        'aid_types': aid_types,
         YEAR_FIELD: request.GET.get(YEAR_GET_FIELD, END_YEAR),
         YEARS_RANGE_FIELD: YEARS,
         'search_form': FacetedProjectSearchForm(request.GET)
