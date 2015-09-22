fc73adfbcba46d177a61dfddf11b296fcc3b1bf7
verganis <stefano.vergani.it@gmail.com>
2015-09-22 18:37:39 +0200

    Export crs update

--- project/openaid/management/commands/export_crs.py
+++ project/openaid/management/commands/export_crs.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 from collections import OrderedDict
 import csv
+from pprint import pprint
 from optparse import make_option
 import time
 from django.core.management.base import BaseCommand
@@ -20,7 +21,7 @@ class Command(BaseCommand):
                     help='Export all activities in one file.'),
     )
 
-    map = {
+    field_map = {
         'year': 'year',
         'pk': 'openaid id',
         'project__agency__donor__code': 'donorcode',
@@ -28,21 +29,47 @@ class Command(BaseCommand):
         'project__agency__code': 'agencycode',
         'project__agency__name': 'agencyname',
         'project__crsid': 'crsid',
-        'project__number': 'projectnumber', #usiamo questo o il number activity come nell'import?
-        'proejct__activity__report_type': 'initialreport',
+        'number': 'projectnumber',
+        'report_type': 'initialreport',
         'project__recipient__code': 'recipientcode',
         'project__recipient__name': 'recipientname',
         'project__recipient__parent__code': 'regioncode',
         'project__recipient__parent__name': 'regioname',
+        'project__recipient__income_group': 'incomegroupcode',
+        # 'project__recipient__income_group__display name': 'incomegroupname', #TODO: use diplay name
+        'flow_type': 'flowcode',
+        #'flow_type__display value': 'flowname', #TODO: use diplay name
+        'bi_multi': 'bi_multi',
+        'project__finance_type': 'finance_t',
+        'project__aid_type': 'aid_t',
+        'commitment_usd': 'usd_commitment',
+        'disbursement_usd': 'usd_disbursement',
+        # '': 'currencycode', #todo: aggiungerlo al csv
+        'commitment': 'commitment_national',
+        'disbursement': 'disbursement_national',
+        # 'project__description': 'shortdescription', #char problem
+        # 'project__title': 'projecttitle', #char problem
+        'project__sector__code': 'purposecode',
+        'project__sector__name': 'purposename',
+        'project__sector__parent__code': 'sectorcode',
+        'project__sector__parent__name': 'sectorname',
+        'project__channel__code': 'channelcode',
+        # 'project__channel__name': 'channelname', #todo: char problem
+        # 'channel_reported__name': 'channelreportedname',#todo: char problem
+        # 'geography': 'geography',#todo: char problem
+        'expected_start_date': 'expectedstartdate',
+        'completion_date': 'completiondate',
+        'long_description': 'longdescription',
+        'project__markers__gender': 'gender',
+        'project__markers__environment': 'environment',
+        'project__markers__trade': 'trade',
+        'project__markers__pd_gg': 'pdgg',
+        'is_ftc': 'FTC',
+        'is_pba': 'PBA',
 
     }
 
-    csv_fieldset = {
-        'pk':'pk',
-        'year':'year',
-        'project__recipient__code':'project__recipient__code',
-        'project__recipient__parent__code':'project__recipient__parent__code'
-    }
+    csv_fieldset = {k:k for k in field_map.keys()}
 
     def write_file(self, activity_set):
         f = open(self.output_filename, "w")
@@ -61,9 +88,9 @@ class Command(BaseCommand):
         activity_set = Activity.objects.all()
         if focus != 'all':
             activity_set = activity_set.filter(year=int(focus))
-        # activity_set = activity_set.values(*self.map.keys())
-        activity_set = activity_set.values(*self.csv_fieldset.keys())
 
+        activity_set = activity_set.values(*self.csv_fieldset.keys())
+        print(len(activity_set))
         self.write_file(activity_set)
 
 
