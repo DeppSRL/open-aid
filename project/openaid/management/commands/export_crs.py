c918c2332d5261468f7ccf0b48bc50b7cf4093fa
verganis <stefano.vergani.it@gmail.com>
2015-09-22 16:45:27 +0200

    Export crs new routine

--- project/openaid/management/commands/export_crs.py
+++ project/openaid/management/commands/export_crs.py
@@ -1,18 +1,71 @@
 # coding=utf-8
+from collections import OrderedDict
+import csv
 from optparse import make_option
 import time
 from django.core.management.base import BaseCommand
-from openaid.management import exporters
+from openaid.utils import UnicodeDictWriter
+from openaid.projects.models import Activity
+
 
 class Command(BaseCommand):
     help = 'Esporta i crs zippati'
+    encoding = 'utf-8'
+    output_filename = 'export.csv'
 
     option_list = BaseCommand.option_list + (
         make_option('-y', '--year', action='store', dest='year', default=None, type=int,
-            help='Select a specific year to export. If not provided one zip for each year will be created.'),
+                    help='Select a specific year to export. If not provided one zip for each year will be created.'),
         make_option('-a', '--all', action='store_true', dest='all', default=False,
-            help='Export all activities in one file.'),
+                    help='Export all activities in one file.'),
     )
+
+    map = {
+        'year': 'year',
+        'pk': 'openaid id',
+        'project__agency__donor__code': 'donorcode',
+        'project__agency__donor__name': 'donorname',
+        'project__agency__code': 'agencycode',
+        'project__agency__name': 'agencyname',
+        'project__crsid': 'crsid',
+        'project__number': 'projectnumber', #usiamo questo o il number activity come nell'import?
+        'proejct__activity__report_type': 'initialreport',
+        'project__recipient__code': 'recipientcode',
+        'project__recipient__name': 'recipientname',
+        'project__recipient__parent__code': 'regioncode',
+        'project__recipient__parent__name': 'regioname',
+
+    }
+
+    csv_fieldset = {
+        'pk':'pk',
+        'year':'year',
+        'project__recipient__code':'project__recipient__code',
+        'project__recipient__parent__code':'project__recipient__parent__code'
+    }
+
+    def write_file(self, activity_set):
+        f = open(self.output_filename, "w")
+
+        udw = UnicodeDictWriter(f, fieldnames=self.csv_fieldset.keys(), encoding=self.encoding)
+
+        udw.writerow(self.csv_fieldset)
+
+        for activity in activity_set:
+            udw.writerow(activity)
+
+
+
+    def export(self, focus):
+
+        activity_set = Activity.objects.all()
+        if focus != 'all':
+            activity_set = activity_set.filter(year=int(focus))
+        # activity_set = activity_set.values(*self.map.keys())
+        activity_set = activity_set.values(*self.csv_fieldset.keys())
+
+        self.write_file(activity_set)
+
 
     def handle(self, *args, **options):
         start_time = time.time()
@@ -24,7 +77,7 @@ class Command(BaseCommand):
             focus = None
         try:
 
-            exporters.export_activities(focus)
+            self.export(focus)
 
         except KeyboardInterrupt:
             self.stdout.write("\nCommand execution aborted.")
--- project/openaid/utils.py
+++ project/openaid/utils.py
@@ -1,5 +1,140 @@
+from _csv import register_dialect, QUOTE_ALL
+import csv
+import codecs
+import cStringIO
+from HTMLParser import HTMLParser
+from itertools import groupby
 from django.conf import settings
 
 
 def currency_converter(amount, year):
     return amount * settings.OPENAID_CURRENCY_CONVERSIONS[year]
+
+
+class UTF8Recoder:
+    """
+    Iterator that reads an encoded stream and reencodes the input to UTF-8
+    """
+
+    def __init__(self, f, encoding):
+        self.reader = codecs.getreader(encoding)(f)
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        return self.reader.next().encode("utf-8")
+
+
+class UnicodeDictReader:
+    """
+    A CSV reader which will iterate over lines in the CSV file "f",
+    which is encoded in the given encoding.
+    """
+
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+        f = UTF8Recoder(f, encoding)
+        self.reader = csv.DictReader(f, dialect=dialect, **kwds)
+
+    def next(self):
+        row = self.reader.next()
+        return dict((k, unicode(s, "utf-8")) for k, s in row.iteritems() if s is not None)
+
+    def __iter__(self):
+        return self
+
+
+class UnicodeDictWriter(object):
+    def __init__(self, f, fieldnames, dialect=csv.excel, encoding="utf-8", **kwds):
+        # Redirect output to a queue
+        self.queue = cStringIO.StringIO()
+        self.writer = csv.DictWriter(self.queue, fieldnames, dialect=dialect, **kwds)
+        self.stream = f
+        self.encoder = codecs.getincrementalencoder(encoding)()
+
+    def writerow(self, D):
+        self.writer.writerow(dict([(k, u"{}".format(v)) for k, v in D.items()]))
+        # Fetch UTF-8 output from the queue ...
+        data = self.queue.getvalue()
+        data = data.decode("utf-8")
+        # ... and reencode it into the target encoding
+        data = self.encoder.encode(data)
+        # write to the target stream
+        self.stream.write(data)
+        # empty queue
+        self.queue.truncate(0)
+
+    def writerows(self, rows):
+        for D in rows:
+            self.writerow(D)
+
+
+class ExcelSemicolon(csv.excel):
+    """Extends excel Dialect in order to set semicolon as delimiter."""
+    delimiter = ';'
+    quoting = QUOTE_ALL
+
+
+register_dialect("excel_semicolon", ExcelSemicolon)
+
+
+class UnicodeWriter:
+    """
+    A CSV writer which will write rows to CSV file "f",
+    which is encoded in the given encoding.
+    """
+
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+        # Redirect output to a queue
+        self.queue = cStringIO.StringIO()
+        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
+        self.stream = f
+        self.encoder = codecs.getincrementalencoder(encoding)()
+
+    def writerow(self, row):
+        self.writer.writerow([s.encode("utf-8") for s in row])
+        # Fetch UTF-8 output from the queue ...
+        data = self.queue.getvalue()
+        data = data.decode("utf-8")
+        # ... and reencode it into the target encoding
+        data = self.encoder.encode(data)
+        # write to the target stream
+        self.stream.write(data)
+        # empty queue
+        self.queue.truncate(0)
+
+    def writerows(self, rows):
+        for row in rows:
+            self.writerow(row)
+
+
+class MLStripper(HTMLParser):
+    def __init__(self):
+        self.reset()
+        self.fed = []
+
+    def handle_data(self, d):
+        self.fed.append(d)
+
+    def get_data(self):
+        return ' '.join(self.fed)
+
+
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
+
+
+# converts a qset into a dict using the key field
+def convert2dict(queryset, key_field):
+    if type(key_field) == tuple or type(key_field) == list:
+        if len(key_field) == 0:
+            raise Exception
+        else:
+            indice_keygen = lambda x: tuple(x[key] for key in key_field)
+
+    else:
+        indice_keygen = lambda x: x[key_field]
+
+    return dict((k, list(v)) for k, v in groupby(queryset, key=indice_keygen))
