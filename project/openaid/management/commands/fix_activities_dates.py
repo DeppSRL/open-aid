001f9484c6d728270ba81bf279100fdce83e6f56
verganis <stefano.vergani.it@gmail.com>
2015-10-06 16:30:29 +0200

    fix activites dates: removed useless function

--- project/openaid/management/commands/fix_activities_dates.py
+++ project/openaid/management/commands/fix_activities_dates.py
@@ -10,10 +10,6 @@ class Command(BaseCommand):
     help = 'Fixes activity dates removing Hours and minutes'
     logger = logging.getLogger('openaid')
 
-    def set_hour_minute_to_zero(self, date):
-        if date is None:
-            return date
-        return date.replace(hour=0,minute=0)
 
     def check_invalid_date(self, date):
         if date is None:
@@ -44,16 +40,12 @@ class Command(BaseCommand):
 
         all_activities = Activity.objects.all().order_by('year')
         for act in all_activities:
-            # fix hour minute for
+            # check valid dates
             # expected_start_date
             # completion_date
             # commitment_date
-            act.expected_start_date = self.set_hour_minute_to_zero(act.expected_start_date)
-            act.completion_date = self.set_hour_minute_to_zero(act.completion_date)
-            act.commitment_date = self.set_hour_minute_to_zero(act.commitment_date)
-            act.save()
             act.expected_start_date = self.check_invalid_date(act.expected_start_date)
             act.completion_date = self.check_invalid_date(act.completion_date)
             act.commitment_date = self.check_invalid_date(act.commitment_date)
-            act.save()
+            # act.save()
         self.logger.info("Finished ")
