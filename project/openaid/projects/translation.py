767befde2be36df3b2c76728b872fac49cc50345
verganis <stefano.vergani.it@gmail.com>
2015-10-19 12:02:39 +0200

    Added translations for fields in Initiatives

--- project/openaid/projects/translation.py
+++ project/openaid/projects/translation.py
@@ -3,12 +3,17 @@ from . import models
 
 
 class InitiativeTranslationOptions(TranslationOptions):
-    fields = ('title', )
+    fields = ('title', 'description_temp', 'outcome_temp', 'beneficiaries_temp', 'other_financiers_temp',
+              'counterpart_authority_temp', 'location_temp' )
+
 
 translator.register(models.Initiative, InitiativeTranslationOptions)
 
+
 class ProjectTranslationOptions(TranslationOptions):
-    fields = ('title', 'description', 'outcome', 'beneficiaries', 'other_financiers', 'counterpart_authority', 'location')
+    fields = (
+        'title', 'description', 'outcome', 'beneficiaries', 'other_financiers', 'counterpart_authority', 'location')
+
 
 translator.register(models.Project, ProjectTranslationOptions)
 
@@ -16,11 +21,13 @@ translator.register(models.Project, ProjectTranslationOptions)
 class ActivityTranslationOptions(TranslationOptions):
     fields = ('title', 'description', 'long_description', )
 
+
 translator.register(models.Activity, ActivityTranslationOptions)
 
 
 class ReportTranslationOptions(TranslationOptions):
     fields = ('description', )
+
 
 translator.register(models.Report, ReportTranslationOptions)
 
@@ -28,11 +35,13 @@ translator.register(models.Report, ReportTranslationOptions)
 class ProblemTranslationOptions(TranslationOptions):
     fields = ('event', 'impact', 'actions')
 
+
 translator.register(models.Problem, ProblemTranslationOptions)
 
 
 class NewProjectTranslationOptions(TranslationOptions):
     fields = ('title', 'description', )
+
 
 translator.register(models.NewProject, NewProjectTranslationOptions)
 
