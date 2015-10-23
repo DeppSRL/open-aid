f7013379fdfd890c3cbb4ecc29321a1acd1f471b
verganis <stefano.vergani.it@gmail.com>
2015-10-23 16:24:56 +0200

    Mng task for import new proj ok

--- project/openaid/management/commands/fill_initiatives_newproj.py
+++ project/openaid/management/commands/fill_initiatives_newproj.py
@@ -1,6 +1,9 @@
 # coding=utf-8
+from django.core.exceptions import ObjectDoesNotExist
+
 __author__ = 'stefano'
 import logging
+from pprint import pprint
 from django.core.management.base import BaseCommand
 from django.db.transaction import set_autocommit, commit
 from openaid.projects.models import Initiative, Project, NewProject
@@ -13,26 +16,60 @@ class Command(BaseCommand):
 
     # maps the field name between new project (keys) and initiative (value)
     field_map = {
+        'title': 'title',
         'description': 'description_temp',
-
+        'code': 'code',
+        'recipient': 'recipient_temp',
     }
 
-    def update_fields(self, initiative):
+    def update_fields(self, new_project, initiative):
         # loops on every field that has to be updated and updates if the conditions apply
-        for project_fieldname, initiative_fieldname in self.field_map.iteritems():
+        for newproject_fieldname, initiative_fieldname in self.field_map.iteritems():
 
-            field_value = initiative._get_first_project_value(project_fieldname)
+            field_value = getattr(new_project, newproject_fieldname)
 
             if field_value is not None:
                 initiative.__setattr__(initiative_fieldname, field_value)
 
         return initiative
 
-    def update_related_objects(self, initiative):
+    def update_related_objects(self, new_project, initiative):
 
         # updates photo set getting the photos from the new projects
-        initiative.photo_set = initiative.photos()
+        initiative.photo_set.add()
+        for pic in new_project.photo_set.all():
+            initiative.photo_set.add(pic)
         return initiative
+
+    def get_code(self,new_project):
+        # gets correct 6 chars code from np.number
+        code = new_project.number.strip()
+        code = code.replace("AID ","")
+        if code is None or code == '':
+            return code
+        code_split_slash = code.split("/")
+        code_split_dot = code.split(".")
+
+        if len(code_split_slash)>1:
+            if len(code_split_slash[0]) <=6:
+                code = code_split_slash[0]
+            else:
+                return -1
+        elif len(code_split_dot)>1:
+            if len(code_split_dot[0]) <=6:
+                code = code_split_dot[0]
+            else:
+                return -1
+        elif len(code) <= 6:
+            code = code
+        else:
+            # malformed code
+            return -1
+        # check that the string contains only numbers
+        if code.isdigit():
+            return code.zfill(6)
+        else:
+            return -1
 
     def handle(self, *args, **options):
         verbosity = options['verbosity']
@@ -45,22 +82,39 @@ class Command(BaseCommand):
         elif verbosity == '3':
             self.logger.setLevel(logging.DEBUG)
 
+        counters = {'null':0, 'existing':0, 'malformed':0, 'processed':0}
 
-        set_autocommit(False)
         self.logger.info(u"Start procedure")
-        for index, new_project in enumerate(NewProject.objects.all().order_by('number')):
-            self.logger.debug(u"Update new proj:'{}'".format(new_project))
+        for index, new_project in enumerate(NewProject.objects.all().order_by('pk')):
+            self.logger.debug(u"Update new proj with pk:'{}'".format(new_project.pk))
 
-            new_project = self.update_fields(new_project)
+            code = self.get_code(new_project)
+            if code is not None and code != '' and code != -1:
+                initiative, created = Initiative.objects.get_or_create(code=code)
+                if created is False:
+                    self.logger.debug(u"Initiative with code:'{}' already exist".format(code))
+                    counters['existing'] += 1
+                else:
+                    self.logger.info(u"Created Initiative with code:'{}'".format(code))
 
-            new_project = self.update_related_objects(new_project)
+            else:
+                if code == None or code == '':
+                    self.logger.error(u"NewProj:{} has code None or ''. Skip".format(new_project.pk))
+                    counters['null'] += 1
+                elif code == -1:
+                    self.logger.error(u"NewProj:{} - Code '{}' is malformed, cannot process. Skip".format(new_project.pk, new_project.number))
+                    counters['malformed'] += 1
+                continue
 
-            #         commits every N initiatives
-            if index % 500 == 0:
-                self.logger.info(u"Reached new proj:'{}'".format(new_project))
-                commit()
+            if initiative is None:
+                self.logger.critical("Initiative cannot be None here. ERROR")
+                exit()
 
-        #             final commit
-        commit()
-        set_autocommit(True)
-        self.logger.info(u"Finished transfering {} new proj".format(NewProject.objects.all().count()))
+            new_project.code = code
+            initiative = self.update_fields(new_project, initiative)
+            initiative = self.update_related_objects(new_project, initiative)
+            initiative.save()
+            counters['processed'] += 1
+
+        self.logger.info(u"Finished transfering {} new proj".format(counters['processed']))
+        pprint(counters)
