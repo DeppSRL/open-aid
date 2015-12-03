cdef222c198b1be1505992a6c2359b953ae19989
verganis <stefano.vergani.it@gmail.com>
2015-12-03 15:13:29 +0100

    Added correct host for ALLOWED HOSTS

--- project/openaid/settings/production.py
+++ project/openaid/settings/production.py
@@ -5,12 +5,9 @@ from .base import *
 
 ########## HOST CONFIGURATION
 # See: https://docs.djangoproject.com/en/1.5/releases/1.5/#allowed-hosts-required-in-production
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['openaid.esteri.it',]
 ########## END HOST CONFIGURATION
 
 ########## EMAIL CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
-
-
