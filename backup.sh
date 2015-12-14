3aa46bdc128cc51099ad3be9cf8888b6fea4479c
verganis <stefano.vergani.it@gmail.com>
2015-12-14 17:00:06 +0100

    Change db name on backup script

--- backup.sh
+++ backup.sh
@@ -9,7 +9,7 @@ DATE_TAG=`date +%Y%m%d`
 BUCKET_NAME=open_aid
 
 echo dumping postgres database ...
-pg_dump --clean -Upostgres open-aid-2013 | gzip > pg_dump.gz
+pg_dump --clean -Upostgres open_aid | gzip > pg_dump.gz
 
 echo dumping solr index files ...
 tar cvzf solr_dump.tgz solr/data/*
