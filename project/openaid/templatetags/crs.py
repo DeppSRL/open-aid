d0e9cff563d46a339a1f98bc59cd9a215934b17b
verganis <stefano.vergani.it@gmail.com>
2015-12-29 16:42:55 +0100

    Drilldown pies working with data

--- project/openaid/templatetags/crs.py
+++ project/openaid/templatetags/crs.py
@@ -86,7 +86,6 @@ def crs_stats(context, instance=None, year=None, show_map=True):
         multi_commitments_sum = multi_projects['multi_commitments_sum']
         multi_disbursements_sum = multi_projects['multi_disbursements_sum']
 
-
         # adds the % commitment and % disbursement for every organization to display in the template
         ctx.update(multi_projects)
 
@@ -95,14 +94,10 @@ def crs_stats(context, instance=None, year=None, show_map=True):
             'total_disbursements_sum': (multi_disbursements_sum or 0.0) + disbursements_sum,
         })
 
-        # ctx.update({
-        #     'multi_stats': projects_models.AnnualFunds.objects.filter(year=year).select_related('organization'),
-        # })
-
-        # drilldown pie code - to activate later #
+        # drilldown pie code
         ctx.update({
-            'multi_stats_commitment': projects_models.AnnualFunds.get_type_distribution(year=year, type='commitment'),
-            'multi_stats_disbursement': projects_models.AnnualFunds.get_type_distribution(year=year, type='disbursement')
+            'multi_stats_commitment': projects_models.AnnualFunds.get_multilateral_data(year=year, type='commitment'),
+            'multi_stats_disbursement': projects_models.AnnualFunds.get_multilateral_data(year=year, type='disbursement')
         })
 
     return ctx
