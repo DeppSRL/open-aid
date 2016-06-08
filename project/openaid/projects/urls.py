0331b13839e6ba3bfa6572d9355ad2bd0ff8c224
joke2k <joke2k@gmail.com>
2016-06-08 06:30:19 +0200

    fix link to initiative

--- project/openaid/projects/urls.py
+++ project/openaid/projects/urls.py
@@ -11,11 +11,7 @@ urls = (
     url(r'^project/list/$', views.ProjectList.as_view(), name='project-list'),
     url(r'^project/(?P<pk>\d+)/$', views.ProjectDetail.as_view(), name='project-detail'),
     # NOTE: search/ is here for back-compatibility
-    url(r'^search/', search_view_factory(
-        views.SearchFacetedProjectView, facets=[
-            'years', 'recipient', 'agencies', 'aid_types', 'channels',
-            'finance_types', 'sectors']
-    ), name='search'),
+    url(r'^search/', search_view_factory(views.SearchFacetedProjectView), name='search'),
     url(r'project/search/', search_view_factory(views.SearchFacetedProjectView), name='project-search'),
     url(r'initiative/search/', search_view_factory(views.SearchFacetedInitiativeView), name='initiative-search'),
 
