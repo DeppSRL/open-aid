2b7110b18b7013441e586b699eb3c122947e9b39
Guglielmo Celata <guglielmo.celata@gmail.com>
2016-09-09 11:30:01 +0200

    modifications restored after reset

--- project/openaid/projects/forms.py
+++ project/openaid/projects/forms.py
@@ -88,3 +88,7 @@ class FacetedProjectSearchForm(FacetedSearchForm):
         Retrieve all search results for empty query string
         """
         return self.searchqueryset.all()
+
+
+class FacetedInitiativeSearchForm(FacetedProjectSearchForm):
+    pass
--- project/openaid/projects/urls.py
+++ project/openaid/projects/urls.py
@@ -10,10 +10,10 @@ urls = (
     url(r'^initiative/(?P<code>\d+)/$', views.InitiativeDetail.as_view(), name='initiative-detail'),
     url(r'^project/list/$', views.ProjectList.as_view(), name='project-list'),
     url(r'^project/(?P<pk>\d+)/$', views.ProjectDetail.as_view(), name='project-detail'),
-    url(r'^search/', search_view_factory(
-        views.SearchFacetedProjectView, facets=[
-            'years', 'recipient', 'agencies', 'aid_types', 'channels',
-            'finance_types', 'sectors']
-    ), name='search'),
+    # NOTE: search/ is here for back-compatibility
+    url(r'^search/', search_view_factory(views.SearchFacetedProjectView), name='search'),
+    url(r'project/search/', search_view_factory(views.SearchFacetedProjectView), name='project-search'),
+    url(r'initiative/search/', search_view_factory(views.SearchFacetedInitiativeView), name='initiative-search'),
+
 )
 urlpatterns = patterns('', *urls)
